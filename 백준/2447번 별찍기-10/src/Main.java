734fb89e91895b29fc8cc220abfaf0eb42e0f22a
khd <kwonhyeokdo1209@gmail.com>
2023-03-14 17:17:25 +0900

    별찍기

--- 백준/2447번 별찍기-10/src/Main.java
+++ 백준/2447번 별찍기-10/src/Main.java
@@ -4,7 +4,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Main {
-    final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private char[][] starArr = null;
+
     public static void main(String[] args) throws Exception {
         new Main().solution();
     }
@@ -13,9 +15,19 @@ public class Main {
         final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         final int N = Integer.parseInt(br.readLine());
+        starArr = new char[N][N];
+
 
         br.close();
         bw.flush();
         bw.close();
+    }
+
+    private final void start(final int startIndex, final int endIndex, final boolean isDraw) throws Exception{
+        final int divide = (endIndex - startIndex + 1) / 3;
+
+        for(int y = startIndex; y < divide * 3; y += divide){
+            
+        }
     }
 }
