60bb6de53621cbfdeb789264001d595635e9206d
khd <kwonhyeokdo1209@gmail.com>
2023-03-15 13:39:09 +0900

    2247번 별찍기-10 완료

--- 백준/2447번 별찍기-10/src/Main.java
+++ 백준/2447번 별찍기-10/src/Main.java
@@ -17,17 +17,48 @@ public class Main {
         final int N = Integer.parseInt(br.readLine());
         starArr = new char[N][N];
 
+        star(0, N, 0, N, true);
+        
+        for(int y = 0; y < N; y++){
+            for(int x = 0; x < N; x++){
+                bw.write(starArr[y][x]);
+            }
+            bw.write("\n");
+        }
 
         br.close();
         bw.flush();
         bw.close();
     }
 
-    private final void start(final int startIndex, final int endIndex, final boolean isDraw) throws Exception{
-        final int divide = (endIndex - startIndex + 1) / 3;
+    private final void star(final int startY, final int endY, final int startX, final int endX, final boolean isDraw) throws Exception{
+        if(endY - startY == 3){
+            for(int y = startY; y < endY; y++){
+                for(int x = startX; x < endX; x++){
+                    if((y == startY + 1 && x == startX + 1) || !isDraw){
+                        starArr[y][x] = ' ';
+                    }else{
+                        starArr[y][x] = '*';
+                    }
+                }
+            }
+            return;
+        }
 
-        for(int y = startIndex; y < divide * 3; y += divide){
-            
+        final int divide = (endY - startY) / 3;
+
+        int y = startY;
+        for(int i = 0; i < 3; i++){
+            int x = startX;
+            for(int j = 0; j < 3; j++){
+                if((i == 1 && j == 1) || !isDraw){
+                    star(x, x + divide, y, y + divide, false);
+                }else{
+                    star(x, x + divide, y, y + divide, true);
+                }
+                x += divide;
+            }
+            y += divide;
         }
     }
 }
