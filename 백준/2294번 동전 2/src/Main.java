c58c15275ca1ebadeec24db6f1a8661e033a2eeb
kwonhyeokdo <kwonhyeokdo1209@gmail.com>
2023-03-20 21:02:26 +0900

    동전 2

--- 백준/2294번 동전 2/src/Main.java
+++ 백준/2294번 동전 2/src/Main.java
@@ -4,26 +4,59 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public final class Main {
+    private int n = 0;
+    private int k = 0;
+    private int[][] memo;
+    private int[] coinArray;
+    private static final int MAX_VALUE = Integer.MAX_VALUE - 1; 
+
     public static final void main(String[] args) throws Exception {
         new Main().solution();
     }
 
-    private void solution() throws Exception{
+    private final void solution() throws Exception{
         final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String[] inputLine = br.readLine().split(" ");
-        final int n = Integer.parseInt(inputLine[0]);
-        final int k = Integer.parseInt(inputLine[1]);
-        final int[] coinArray = new int[n];
+        n = Integer.parseInt(inputLine[0]);
+        k = Integer.parseInt(inputLine[1]);
+        coinArray = new int[n];
         for(int i = 0; i < n; i++){
             coinArray[i] = Integer.parseInt(br.readLine());
         }
-
+        memo = new int[n + 1][k + 1];
+        for(int i = 0; i < n + 1; i++){
+            for(int j = 0; j < k + 1; j++){
+                memo[i][j] = -1;
+            }
+        }
         
+        int answer = dp(0, k);
+        if(answer == MAX_VALUE){
+            answer = -1;
+        }
+
+        bw.write(String.valueOf(answer));
 
         br.close();
         bw.flush();
         bw.close();
+    }
+
+    private final int dp(final int n, final int k){
+        if(n == this.n){
+            return (k == 0 ? 0 : MAX_VALUE);
+        }
+        if(memo[n][k] != -1){
+            return memo[n][k];
+        }
+
+        int result = dp(n + 1, k);
+        if(coinArray[n] <= k){
+            result = Math.min(result, dp(n, k - coinArray[n]) + 1);
+        }
+        memo[n][k] = result;
+        return result;
     }
 }
