b132bc4fd9c22859f01015a202222101dbc5ac18
kwonhyeokdo <kwonhyeokdo1209@gmail.com>
2023-03-28 21:16:25 +0900

    11052번 카드 구매하기

--- 백준/11052번 카드 구매하기/src/Main.java
+++ 백준/11052번 카드 구매하기/src/Main.java
@@ -4,6 +4,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Main {
+    private int[] cardPack;
+    private int[] memo;
+
     public static void main(String[] args) throws Exception {
         new Main().solution();
     }
@@ -13,16 +16,36 @@ public class Main {
         final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         final int N = Integer.parseInt(br.readLine());
+        memo = new int[N + 1];
         final String[] inputLine = br.readLine().split(" ");
-        final double[] cardPackRank = new double[N];
-        for(int i = 0; i < inputLine.length; i++){
-            cardPackRank[i] = (i + 1) / Double.parseDouble(inputLine[i]);
+        cardPack = new int[N + 1];
+        for(int i = 0; i < N; i++){
+            cardPack[i + 1] = Integer.parseInt(inputLine[i]);
         }
-        
 
+        final int answer = dp(N);
+
+        bw.write(String.valueOf(answer));
 
         br.close();
         bw.flush();
         bw.close();
+    }
+
+    private final int dp(final int n){
+        int result = 0;
+
+        if(memo[n] != 0){
+            return memo[n];
+        }
+
+        result = cardPack[n];
+        for(int i = 1; i < n; i++){
+            result = Math.max(result, dp(i) + dp(n - i));
+        }
+
+        memo[n] = result;
+
+        return result;
     }
 }
