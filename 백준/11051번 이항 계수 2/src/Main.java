46b6d1ca27b41bb939c1445d1e47426daccd0478
kwonhyeokdo <kwonhyeokdo1209@gmail.com>
2023-04-02 16:33:04 +0900

    11051번 이항 계수 2

--- 백준/11051번 이항 계수 2/src/Main.java
+++ 백준/11051번 이항 계수 2/src/Main.java
@@ -4,6 +4,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public final class Main {
+    private int[][] memo = null;
+    private static final int MOD = 10007;
+
     public static void main(String[] args) throws Exception {
         new Main().solution();
     }
@@ -12,12 +15,31 @@ public final class Main {
         final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        final int N = Integer.parseInt(br.readLine());
-        final int K = Integer.parseInt(br.readLine());
+        final String[] input = br.readLine().split(" ");
+        final int N = Integer.parseInt(input[0]);
+        final int K = Integer.parseInt(input[1]);
+        memo = new int[N + 1][K + 1];
 
+        final int answer = pascal(N, K);
+
+        bw.write(String.valueOf(answer));
 
         bw.flush();
         bw.close();
         br.close();
+    }
+
+    private final int pascal(final int n, final int k){
+        if(n == k || k == 0){
+            return 1;
+        }
+        
+        if(memo[n][k] != 0){
+            return memo[n][k];
+        }
+
+        memo[n][k] = (pascal(n - 1, k - 1) % MOD + pascal(n - 1, k) % MOD) % MOD;
+
+        return memo[n][k];
     }
 }
