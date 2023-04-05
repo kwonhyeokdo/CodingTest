49c514e8defa304d2aec22a93b1d7c397cba6cef
khd <kwonhyeokdo1209@gmail.com>
2023-04-05 17:31:14 +0900

    셔플 문제

--- 셔플 문제/src/SettingProperties.java
+++ 셔플 문제/src/SettingProperties.java
@@ -12,4 +12,36 @@ public class SettingProperties {
         this.outputDirectory = outputDirectory;
         this.outputFileNames = outputFileNames;
     }
+
+    public ImportBaseEnum getImportBase() {
+        return importBase;
+    }
+
+    public void setImportBase(ImportBaseEnum importBase) {
+        this.importBase = importBase;
+    }
+
+    public String[] getImportContents() {
+        return importContents;
+    }
+
+    public void setImportContents(String[] importContents) {
+        this.importContents = importContents;
+    }
+
+    public String getOutputDirectory() {
+        return outputDirectory;
+    }
+
+    public void setOutputDirectory(String outputDirectory) {
+        this.outputDirectory = outputDirectory;
+    }
+
+    public String[] getOutputFileNames() {
+        return outputFileNames;
+    }
+
+    public void setOutputFileNames(String[] outputFileNames) {
+        this.outputFileNames = outputFileNames;
+    }
 }
