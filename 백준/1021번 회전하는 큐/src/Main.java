ec12dcc195e08d3d89aa8c99a09c49b8939d4729
khd <kwonhyeokdo1209@gmail.com>
2023-05-19 11:08:57 +0900

    1021번 회전하는 큐

--- 백준/1021번 회전하는 큐/src/Main.java
+++ 백준/1021번 회전하는 큐/src/Main.java
@@ -3,7 +3,6 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
