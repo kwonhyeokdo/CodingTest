90ed922db66fb6c10b98dbf08e32bd2c5a3cb770
khd <kwonhyeokdo1209@gmail.com>
2023-05-22 17:30:11 +0900

    3190번 뱀

--- 백준/3190번 뱀/src/Main.java
+++ 백준/3190번 뱀/src/Main.java
@@ -2,9 +2,12 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public final class Main{
@@ -18,55 +21,84 @@ public final class Main{
 
         final int N = Integer.parseInt(br.readLine());
         final int K = Integer.parseInt(br.readLine());
-        final Set<int[]> apples = new HashSet<>();
+        int[][] board = new int[N + 2][N + 2];
         for(int i = 0; i < K; i++){
             final String[] inputApplePos = br.readLine().split(" ");
-            final int x = Integer.parseInt(inputApplePos[0]);
-            final int y = Integer.parseInt(inputApplePos[1]);
-            apples.add(new int[]{x, y});
+            final int y = Integer.parseInt(inputApplePos[0]);
+            final int x = Integer.parseInt(inputApplePos[1]);
+            board[y][x] = 2;
         }
         final int L = Integer.parseInt(br.readLine());
-        final String[][] direct = new String[L][2];
+        final Map<Integer, String> direct = new HashMap<>();
         for(int i = 0; i < L; i++){
-            direct[i] = br.readLine().split(" ");
+            final String[] inputTimeDir = br.readLine().split(" ");
+            direct.put(Integer.parseInt(inputTimeDir[0]), inputTimeDir[1]);
         }
 
         int answer = 0;
-        boolean isDie = true;
         final LinkedList<int[]> snake = new LinkedList<>();
         snake.add(new int[]{1, 1, 3});
-        boolean[][] board = new boolean[N + 1][N + 1];
-        board[1][1] = true;
-        while(!isDie){
+        board[1][1] = 1;
+        while(true){
             answer++;
 
-            final int[] head = snake.getFirst();
-            switch(head[2]){
-                case 0:
-                    head[1]--;
-                    break;
-                case 1:
-                    head[1]++;
-                    break;
-                case 2:
-                    head[0]--;
-                    break;
-                default:
-                    head[0]++;
-                    break;
+            int[] head = snake.getFirst();
+            int x = head[0];
+            int y = head[1];
+            int dir = head[2];
+            if(dir == 0){
+                y--;
+            }else if(dir == 1){
+                y++;
+            }else if(dir == 2){
+                x--;
+            }else{
+                x++;
             }
-            if(head[0] < 1 || head[0] > N || head[1] < 1 || head[1] > N){
-                break;
-            }
-            if(board[head[1]][head[0]]){
+
+            // 맵 밖에 나가면 죽음
+            if(x < 1 || x > N || y < 1 || y > N){
                 break;
             }
 
-            final int[] moveHead = {head[0], head[1]};
-            if(apples.contains(moveHead)){
-                apples.remove(moveHead);
-            }else{
-                
+            // 머리가 몸통에 닿음
+            if(board[y][x] == 1){
+                break;
+            }
+
+            final int[] newHead = {x, y, dir};
+            snake.addFirst(newHead);
+            head = newHead;
+            if(board[y][x] != 2){
+                final int[] tail = snake.removeLast();
+                board[tail[1]][tail[0]] = 0;
+            }
+            
+            board[y][x] = 1;
+
+            if(direct.containsKey(answer)){
+                final String C = direct.get(answer);
+                if("L".equals(C)){
+                    if(dir == 0){
+                        head[2] = 2;
+                    }else if(dir == 1){
+                        head[2] = 3;
+                    }else if(dir == 2){
+                        head[2] = 1;
+                    }else{
+                        head[2] = 0;
+                    }
+                }else{
+                    if(dir == 0){
+                        head[2] = 3;
+                    }else if(dir == 1){
+                        head[2] = 2;
+                    }else if(dir == 2){
+                        head[2] = 0;
+                    }else{
+                        head[2] = 1;
+                    }
+                }
             }
         }
 
