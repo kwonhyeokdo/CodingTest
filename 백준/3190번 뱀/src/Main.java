79995b60fdbb1ca24af22164d8ae5908ebbe4e84
khd <kwonhyeokdo1209@gmail.com>
2023-05-23 16:55:22 +0900

    3190번 뱀

--- 백준/3190번 뱀/src/Main.java
+++ 백준/3190번 뱀/src/Main.java
@@ -2,13 +2,9 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public final class Main{
     public static void main(String[] args) throws Exception{
