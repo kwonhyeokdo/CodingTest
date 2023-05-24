04fa712325addd4a6c4fbc98c7451c325c9f5485
khd <kwonhyeokdo1209@gmail.com>
2023-05-24 14:57:43 +0900

    3078번 좋은 친구

---
+++ 백준/3078번 좋은 친구/src/Main.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public final class Main{
+    public static void main(String[] args) throws Exception{
+        new Main().solution();
+    }
+    
+    private final void solution() throws Exception{
+        final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        final String[] inputNK = br.readLine().split(" ");
+        final int N = Integer.parseInt(inputNK[0]);
+        final int K = Integer.parseInt(inputNK[1]);
+        List<Queue<Integer>> queList = new ArrayList<>();
+        for(int i = 0; i <= 20; i++){
+            queList.add(new LinkedList<>());
+        }
+        for(int i = 0; i < N; i++){
+            final int len = br.readLine().length();
+            queList.get(len).add(i);
+        }
+        for(int i = 0; i <= 20; i++){
+            System.out.println(queList.get(i));
+        }
+
+        long answer = 0;
+
+        bw.write(String.valueOf(answer));
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
