291d5fd76bb989ce16d8a9665d9a8d6ec3cfa4f6
khd <kwonhyeokdo1209@gmail.com>
2023-05-24 10:21:44 +0900

    5076번 Web Pages

--- 백준/5076번 Web Pages/src/Main.java
+++ 백준/5076번 Web Pages/src/Main.java
@@ -22,13 +22,52 @@ public final class Main{
             boolean isAnswer = true;
 
             final Stack<String> stack = new Stack<>();
-            boolean isOpen = false;
-            for(int i = 0; i < inputChars.length; i++){
-                if(i < inputChars.length - 2){
-                    
-                } 
+            for(int i = 0; i < inputChars.length - 1; i++){
+                char c1 = inputChars[i];
+                char c2 = inputChars[i + 1];
+                if(c1 == '<' && c2 != '/'){
+                    StringBuffer sb = new StringBuffer();
+                    boolean isKeyword = true;
+                    for(int j = i + 1; j < inputChars.length; j++){
+                        if(inputChars[j] == '/' && j < inputChars.length - 1){
+                            if(inputChars[j + 1] == '>'){
+                                i = j + 1;
+                                break;
+                            }
+                        }
+                        if(inputChars[j] == '>'){
+                            stack.push(sb.toString());
+                            i = j;
+                            break;
+                        }
+                        if(isKeyword && inputChars[j] == ' '){
+                            isKeyword = false;
+                        }
+                        if(isKeyword){
+                            sb.append(inputChars[j]);
+                        }
+                    }
+                }else if(c1 == '<' && c2 == '/'){
+                    StringBuffer sb = new StringBuffer();
+                    for(int j = i + 2; j < inputChars.length; j++){
+                        if(inputChars[j] == '>'){
+                            final String keyword = sb.toString();
+                            if(!stack.isEmpty() && stack.peek().equals(keyword)){
+                                stack.pop();
+                                i = j;
+                            }else{
+                                isAnswer = false;
+                            }
+                            break;
+                        }
+                        sb.append(inputChars[j]);
+                    }
+                }
             }
-            
+
+            if(isAnswer && !stack.isEmpty()){
+                isAnswer = false;
+            }
 
             if(isAnswer){
                 bw.write("legal\n");
